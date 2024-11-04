5346ca2b1f33449280726bc6793644bb1f5192fe
adam <adam.edward.gorka@gmail.com>
2024-11-04 16:44:04 -0500

    Initial Commit

---
+++ MotionMend/MotionMendApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionMendApp.swift
+//  MotionMend
+//
+//  Created by agorka on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MotionMendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotionMendTests/MotionMendTests.swift
@@ -0,0 +1,16 @@
+//
+//  MotionMendTests.swift
+//  MotionMendTests
+//
+//  Created by agorka on 11/4/24.
+//
+
+import Testing
+
+struct MotionMendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
